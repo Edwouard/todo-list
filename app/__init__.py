cf091477601c770f8942863c47618fc5dba818d0
Yao <128631389+Edwouard@users.noreply.github.com>
2025-01-23 19:05:46 +0000

    Intégration avec mongodb (#4)
    - Ajout de la classe MongoDB
    - Implementation des opérations de base de données
    - Ajout des test de connexion
    - Mise à jour du script d'initialisation de l'application

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
 from config import Config
+from app.database import MongoDatabase
+
+mongo = MongoDatabase()
+
 
 def create_app(config_class=Config):
-    app = Flask(__name__)  
+    app = Flask(__name__)
     app.config.from_object(config_class)
+
+    # Initialize MongoDB
+    mongo.init_app(app)
+
+    # Register blueprints
+    from app.routes import main
+
+    app.register_blueprint(main)
+
     return app
