48ddb8f3fced1fb3021c2b47bfd262be58c2019e
Yao AMEGANDJIN <yaogameliamegandjin@gmail.com>
2025-01-23 18:42:59 +0000

    Flask : implementation des opérations CRUD - Ajout des routes s - Implémentation des requêtes de base create/read/update/delete - Ajout de tests basics

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import Config
+
 def create_app(config_class=Config):
-    app = Flask(_name_)  
+    app = Flask(__name__)  
     app.config.from_object(config_class)
     return app
