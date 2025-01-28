2495e524be831834ca30011e53962e5a6f1a95e4
Yao <128631389+Edwouard@users.noreply.github.com>
2025-01-28 11:55:51 +0000

    Mise en place de la conteurisation Docker (#6)
    - Ajout du Dockerfile pour l'application Flask
    - Configuration docker-compose pour Flask et MongoDB
    - Scripts d'initialisation de la base de données
    - Documentation pour le déploiement Docker

--- config.py
+++ config.py
@@ -5,17 +5,19 @@ load_dotenv()
 
 
 class Config:
-    # Configuration pour MongoDB local
+    # Configuration serveur Flask
+    HOST = os.environ.get("FLASK_RUN_HOST", "0.0.0.0")
+    PORT = int(os.environ.get("FLASK_RUN_PORT", 5000))
+    DEBUG = os.environ.get("FLASK_ENV") == "development"
+
+    # Configuration MongoDB
     MONGO_HOST = os.environ.get("MONGO_HOST", "localhost")
     MONGO_PORT = int(os.environ.get("MONGO_PORT", 27017))
     MONGO_USER = os.environ.get("MONGO_USER", "todo_user")
     MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "passer")
     MONGO_DB = os.environ.get("MONGO_DB", "todo_db")
 
-    # URI MongoDB
     MONGO_URI = os.environ.get(
         "MONGO_URI",
-        f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?authSource=admin",
+        f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?authSource=todo_db",
     )
-
-    DEBUG = True
